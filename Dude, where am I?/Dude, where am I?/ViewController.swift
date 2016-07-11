45e6cfa5301f3434ff91e715a7bf648c1f2a3707
Chris Myers <cmyers78@gmail.com>
2016-07-11 15:45:45 -0600

    initial map view set up.

---
+++ Dude, where am I?/Dude, where am I?/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dude, where am I?
+//
+//  Created by Christopher Myers on 7/11/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
