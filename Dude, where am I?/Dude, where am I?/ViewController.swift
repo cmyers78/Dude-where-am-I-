23f842fd34d6f37c4ba670f005ed714b96122740
Chris Myers <cmyers78@gmail.com>
2016-07-11 21:35:49 -0600

    will find current location and update current location.

--- Dude, where am I?/Dude, where am I?/ViewController.swift
+++ Dude, where am I?/Dude, where am I?/ViewController.swift
@@ -24,16 +24,52 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestWhenInUseAuthorization()
         
+        self.findLocation()
     }
 
     
     
-    
-    @IBAction func findLocation(sender: UIBarButtonItem) {
+    func findLocation() {
         
+        let status = CLAuthorizationStatus.AuthorizedWhenInUse
+        
+        if status != .Denied {
+            self.mapView.showsUserLocation = true
+            self.locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        self.findLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count > 0 {
+            let location = locations.first
+            print(location?.coordinate.latitude)
+            print(location?.coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            if let center = location?.coordinate {
+                let region = MKCoordinateRegion(center: center, span: span)
+                
+                self.mapView.setRegion(region, animated: true)
+                self.mapView.showsUserLocation = true
+            }
+        }
+        print("location updated")
         
     }
     
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.localizedDescription)
+    }
+    
+    
+    @IBAction func updateLocation(sender: UIBarButtonItem) {
+        
+        self.findLocation()
+    }
     
     
     @IBAction func pinMePressed(sender: UIBarButtonItem) {
